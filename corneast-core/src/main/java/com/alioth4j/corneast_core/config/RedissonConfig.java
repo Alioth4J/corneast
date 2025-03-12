f8d5c49d73f5c24c7bfb4d164d735ecf6c757ebc
Alioth Null <alioth4j@proton.me>
2025-03-12 11:07:09 +0800

    feat: redisson

---
+++ corneast-core/src/main/java/com/alioth4j/corneast_core/config/RedissonConfig.java
@@ -0,0 +1,21 @@
+package com.alioth4j.corneast_core.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class RedissonConfig {
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() throws IOException {
+        Config config = Config.fromYAML(new ClassPathResource("redisson-config.yml").getInputStream());
+        return Redisson.create(config);
+    }
+
+}
