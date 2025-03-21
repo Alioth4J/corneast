27c57565f6d11ee17788256167a18fdef4050da8
Alioth Null <alioth4j@proton.me>
2025-03-21 11:30:21 +0800

    fix: provide no args constructor for serialization

--- corneast-core/src/main/java/com/alioth4j/corneast_core/pojo/ReduceReqDTO.java
+++ corneast-core/src/main/java/com/alioth4j/corneast_core/pojo/ReduceReqDTO.java
@@ -2,10 +2,12 @@ package com.alioth4j.corneast_core.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ReduceReqDTO implements Serializable {
 
