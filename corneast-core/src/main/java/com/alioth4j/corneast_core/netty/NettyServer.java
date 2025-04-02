1c09741902a47d18c32342b3b6fc2d7d1cf6f30a
Alioth Null <alioth4j@proton.me>
2025-04-02 16:00:43 +0800

    optimize: netty

--- corneast-core/src/main/java/com/alioth4j/corneast_core/netty/NettyServer.java
+++ corneast-core/src/main/java/com/alioth4j/corneast_core/netty/NettyServer.java
@@ -2,9 +2,8 @@ package com.alioth4j.corneast_core.netty;
 
 import com.alioth4j.corneast_core.proto.ReduceProto;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -39,12 +38,18 @@ public class NettyServer {
         new Thread(() -> {
             // TODO adjust params
             bossGroup = new NioEventLoopGroup(1);
-            workerGroup = new NioEventLoopGroup();
+            workerGroup = new NioEventLoopGroup(80); // 4 * (CPU cores)
             try {
                 ServerBootstrap bootstrap = new ServerBootstrap();
                 bootstrap.group(bossGroup, workerGroup)
                         .channel(NioServerSocketChannel.class)
+                        .option(ChannelOption.SO_BACKLOG, 1024)
+                        .option(ChannelOption.TCP_NODELAY, true)
+                        .option(ChannelOption.SO_REUSEADDR, true)
                         .handler(new LoggingHandler(LogLevel.INFO))
+                        .childOption(ChannelOption.SO_KEEPALIVE, false)
+                        .childOption(ChannelOption.ALLOCATOR, new PooledByteBufAllocator(true, 40, 40, 8192, 11))
+                        .childOption(ChannelOption.RCVBUF_ALLOCATOR, new AdaptiveRecvByteBufAllocator(64, 8192, 65535))
                         .childHandler(new ChannelInitializer<SocketChannel>() {
                             @Override
                             protected void initChannel(SocketChannel ch) {
