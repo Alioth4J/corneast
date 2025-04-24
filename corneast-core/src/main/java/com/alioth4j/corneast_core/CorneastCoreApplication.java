febc669a585f251dec6b648efb074c3ee5b69e50
Alioth Null <alioth4j@proton.me>
2025-04-24 20:45:45 +0800

    feat: log

--- corneast-core/src/main/java/com/alioth4j/corneast_core/CorneastCoreApplication.java
+++ corneast-core/src/main/java/com/alioth4j/corneast_core/CorneastCoreApplication.java
@@ -2,6 +2,7 @@ package com.alioth4j.corneast_core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
  */
 @SpringBootApplication
 @EnableAsync
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class CorneastCoreApplication {
 
 	public static void main(String[] args) {
---
+++ corneast-core/src/main/java/com/alioth4j/corneast_core/aop/RequestLoggingAspect.java
@@ -0,0 +1,64 @@
+package com.alioth4j.corneast_core.aop;
+
+import com.alioth4j.corneast_core.proto.RequestProto;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Aspect class for request handling logging.
+ *
+ * @author Alioth Null
+ */
+@Aspect
+@Component
+public class RequestLoggingAspect {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestLoggingAspect.class);
+
+    @Around("execution(* com.alioth4j.corneast_core.strategy.impl.RegisterRequestHandlingStrategy.handle(..))")
+    public Object logRegisterRequest(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        RequestProto.RequestDTO requestDTO = (RequestProto.RequestDTO) args[0];
+        log.info("Received request: type={}, key={}, tokenCount={}", requestDTO.getType(), requestDTO.getRegisterReqDTO().getKey(), requestDTO.getRegisterReqDTO().getTokenCount());
+        try {
+            Object result = joinPoint.proceed();
+            return result;
+        } catch (Throwable t) {
+            log.error("Error processing request: type={}, key={}, tokenCount={}", requestDTO.getType(), requestDTO.getRegisterReqDTO().getKey(), requestDTO.getRegisterReqDTO().getTokenCount());
+            throw t;
+        }
+    }
+
+    @Around("execution(* com.alioth4j.corneast_core.strategy.impl.ReduceRequestHandlingStrategy.handle(..))")
+    public Object logReduceRequest(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        RequestProto.RequestDTO requestDTO = (RequestProto.RequestDTO) args[0];
+        log.info("Received request: type={}, key={}", requestDTO.getType(), requestDTO.getReduceReqDTO().getKey());
+        try {
+            Object result = joinPoint.proceed();
+            return result;
+        } catch (Throwable t) {
+            log.error("Error processing request: type={}, key={}", requestDTO.getType(), requestDTO.getReduceReqDTO().getKey());
+            throw t;
+        }
+    }
+
+    @Around("execution(* com.alioth4j.corneast_core.strategy.impl.QueryRequestHandlingStrategy.handle(..))")
+    public Object logQueryRequest(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] args = joinPoint.getArgs();
+        RequestProto.RequestDTO requestDTO = (RequestProto.RequestDTO) args[0];
+        log.info("Received request: type={}, key={}", requestDTO.getType(), requestDTO.getQueryReqDTO().getKey());
+        try {
+            Object result = joinPoint.proceed();
+            return result;
+        } catch (Throwable t) {
+            log.error("Error processing request: type={}, key={}", requestDTO.getType(), requestDTO.getRegisterReqDTO().getKey());
+            throw t;
+        }
+    }
+
+}
