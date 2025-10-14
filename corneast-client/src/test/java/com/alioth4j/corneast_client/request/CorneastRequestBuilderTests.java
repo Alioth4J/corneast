a0fd9d269589eb33e9417674f005c6876786c1f7
Alioth Null <alioth4j@proton.me>
2025-10-15 00:19:33 +0800

    Remove deprecated comment

--- corneast-client/src/test/java/com/alioth4j/corneast_client/request/CorneastRequestBuilderTests.java
+++ corneast-client/src/test/java/com/alioth4j/corneast_client/request/CorneastRequestBuilderTests.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-// TODO add tests
-
 /**
  * Test class for CorneastRequestBuilder.
  *
