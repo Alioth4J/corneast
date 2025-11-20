62cd9e8a6c1bad3c1c8262f4c9311c609fe9a59e
Alioth Null <alioth4j@proton.me>
2025-11-20 19:29:23 +0800

    add corneast-example module

---
+++ corneast-example/src/main/java/com/alioth4j/corneast_example/CorneastExampleApplication.java
@@ -0,0 +1,13 @@
+package com.alioth4j.corneast_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorneastExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorneastExampleApplication.class, args);
+    }
+
+}
---
+++ corneast-example/src/test/java/com/alioth4j/corneast_example/CorneastExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alioth4j.corneast_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorneastExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
