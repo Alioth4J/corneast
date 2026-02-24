0d79d0f5779aadd14ef940a36e1890d8af2da8cc
Alioth Null <alioth4j@proton.me>
2026-02-25 00:07:41 +0800

    Fix module exports of corneast-client

--- corneast-client/src/main/java/module-info.java
+++ corneast-client/src/main/java/module-info.java
@@ -2,4 +2,10 @@ module com.alioth4j.corneast.client {
     requires com.alioth4j.corneast.common;
     requires eureka.client;
     requires org.slf4j;
+
+    exports com.alioth4j.corneast.client.config;
+    exports com.alioth4j.corneast.client.eureka;
+    exports com.alioth4j.corneast.client.exception;
+    exports com.alioth4j.corneast.client.request;
+    exports com.alioth4j.corneast.client.send;
 }
