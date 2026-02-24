6003b2ac4e9be2e68a62cbd28fa127ec4fbb14e3
Alioth Null <alioth4j@proton.me>
2026-02-24 21:41:38 +0800

    Update copyleft

--- corneast-client/src/main/java/com/alioth4j/corneast/client/send/CorneastBioClient.java
+++ corneast-client/src/main/java/com/alioth4j/corneast/client/send/CorneastBioClient.java
@@ -1,6 +1,6 @@
 /*
  * Corneast
- * Copyright (C) 2025 Alioth Null
+ * Copyright (C) 2025-2026 Alioth Null
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
