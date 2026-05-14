3e3bc2e88d3aea9a0b7277fecbc05d0d3e9ac9ba
Alioth Null <alioth4j@proton.me>
2026-05-14 22:42:12 +0800

    Replace sh with bash

--- corneast-deploy/docker-rebuild.sh
+++ corneast-deploy/docker-rebuild.sh
@@ -12,9 +12,9 @@ docker ps -a -q -f "name=^corneast" | xargs -r docker rm
 { docker network ls -q -f name='corneast-idempotent-network' | grep -q . && docker network rm corneast-idempotent-network; } || true
 
 # create idempotent instances
-sh "${SCRIPT_DIR}/deploy-idempotent.sh" 6
+bash "${SCRIPT_DIR}/deploy-idempotent.sh" 6
 
 # create storage instances
 docker network create corneast-storage-network
-sh "${SCRIPT_DIR}/deploy-storage-clusters.sh" 2 3
-sh "${SCRIPT_DIR}/deploy-storage-sentinels.sh" 3
+bash "${SCRIPT_DIR}/deploy-storage-clusters.sh" 2 3
+bash "${SCRIPT_DIR}/deploy-storage-sentinels.sh" 3
